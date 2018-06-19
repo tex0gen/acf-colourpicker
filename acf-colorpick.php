fb3f1bec39c800b889d16a4f3104331c7bd5193b
Steve <tex0gen@gmail.com>
2018-06-19 21:24:14 +0100

    Initial Commit

---
+++ acf-colorpick.php
@@ -0,0 +1,90 @@
+<?php
+
+/*
+Plugin Name: Advanced Custom Fields: External Colour Picker
+Plugin URI: https://apexdevs.io/
+Description: Select colours from external file. This will prevent having to edit each background colour field sets.
+Version: 1.0.0
+Author: Tex0gen
+Author URI: https://apexdevs.io
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+*/
+
+// exit if accessed directly
+if( ! defined( 'ABSPATH' ) ) exit;
+
+
+// check if class already exists
+if( !class_exists('colorpick_ext_acf_field_colorpick') ) :
+
+class colorpick_ext_acf_field_colorpick {
+	
+	// vars
+	var $settings;
+	
+	
+	/*
+	*  __construct
+	*
+	*  This function will setup the class functionality
+	*
+	*  @type	function
+	*  @date	17/02/2016
+	*  @since	1.0.0
+	*
+	*  @param	void
+	*  @return	void
+	*/
+	
+	function __construct() {
+		
+		// settings
+		// - these will be passed into the field class.
+		$this->settings = array(
+			'version'	=> '1.0.0',
+			'url'		=> plugin_dir_url( __FILE__ ),
+			'path'		=> plugin_dir_path( __FILE__ )
+		);
+		
+		
+		// include field
+		add_action('acf/include_field_types', 	array($this, 'include_field')); // v5
+		add_action('acf/register_fields', 		array($this, 'include_field')); // v4
+	}
+	
+	
+	/*
+	*  include_field
+	*
+	*  This function will include the field type class
+	*
+	*  @type	function
+	*  @date	17/02/2016
+	*  @since	1.0.0
+	*
+	*  @param	$version (int) major ACF version. Defaults to 4
+	*  @return	void
+	*/
+	
+	function include_field( $version = 4 ) {
+		
+		// load textdomain
+		load_plugin_textdomain( 'TEXTDOMAIN', false, plugin_basename( dirname( __FILE__ ) ) . '/lang' ); 
+		
+		
+		// include
+		include_once('fields/class-colorpick_ext-acf-field-colorpick-v' . $version . '.php');
+	}
+	
+}
+
+
+// initialize
+new colorpick_ext_acf_field_colorpick();
+
+
+// class_exists check
+endif;
+	
+?>
